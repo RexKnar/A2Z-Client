120c82d09f46061b57269ad509e6c3063fc9f8c1
Beena2104 <beenadasc@gmail.com>
2020-07-24 11:00:28 +0530

    error while compile

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { SharedModule } from './shared/shared.module';
 @NgModule({
    declarations: [
       AppComponent
@@ -13,7 +13,8 @@ import { AppComponent } from './app.component';
       BrowserModule,
       AppRoutingModule,
       CoreModule,
-      HomeModule
+      SharedModule,
+      HomeModule,
    ],
    providers: [],
    bootstrap: [
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { SharedComponent } from './shared.component';
-
+import { FooterComponent } from './components/footer/footer.component';
+import { RouterModule } from '@angular/router';
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule,
   ],
-  declarations: [SharedComponent]
+  declarations: [FooterComponent]
 })
 export class SharedModule { }
