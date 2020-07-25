6e4abd01910d7aded7b04e19b97c05a5d96a48c1
Beena2104 <beenadasc@gmail.com>
2020-07-25 18:05:21 +0530

    m

--- src/app/features/home/home.module.ts
+++ src/app/features/home/home.module.ts
@@ -2,10 +2,12 @@ import { HomeRoutes } from './home.routing';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 @NgModule({
   imports: [
     CommonModule,
+    SharedModule,
     HomeRoutes
   ],
   declarations: [HomeComponent]
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FooterComponent } from './components/footer/footer.component';
 import { RouterModule } from '@angular/router';
+import { ProductTabComponent } from './components/product-tab/product-tab.component';
 @NgModule({
   imports: [
     CommonModule,
     RouterModule,
   ],
-  declarations: [FooterComponent],
+  declarations: [FooterComponent, ProductTabComponent],
   exports: [
     FooterComponent
   ]
