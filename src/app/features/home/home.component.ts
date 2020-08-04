6a8bbe7e31d68b8a926016c76f2bfbab69f12d20
jenkins <afjenkins11@gmail.com>
2020-08-04 12:23:35 +0530

    slider-code

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,6 +1,4 @@
 import { Slider } from './../../shared/models/slider';
-
-
 import { Observable } from 'rxjs';
 import { Component, OnInit } from '@angular/core';
 import { Banner } from 'src/app/shared/models/Banner';
@@ -14,26 +12,17 @@ import { SliderService } from 'src/app/shared/services/slider.service';
 })
 export class HomeComponent implements OnInit {
 
-  
 
-
-
-  
   constructor(private _bannerService: BannerService,
-    private _sliderService: SliderService
-    ) {}
-    slider1Data: Observable<Slider[]>;
-  
-
- 
+    private _sliderService: SliderService) 
+    {}
+  slider1Data: Observable<Slider[]>;
   topBannerData: Observable<Banner[]>;
   bottomBannerData: Observable<Banner[]>;
 
   ngOnInit() {
     this.topBannerData = this._bannerService.getBanner('banner1');
     this.bottomBannerData = this._bannerService.getBanner('banner2');
-  
-  this.slider1Data = this._sliderService.getSlider('slider1');
-  
+    this.slider1Data = this._sliderService.getSlider('slider1');
   }
 }
