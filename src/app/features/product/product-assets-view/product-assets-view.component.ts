c0a35e82d3095bf629c76c52bacc0a5f68242034
rajadurai2497 <rajadurai2497@gmail.com>
2020-08-06 13:48:20 +0530

    productassetview

--- src/app/features/product/product-assets-view/product-assets-view.component.ts
+++ src/app/features/product/product-assets-view/product-assets-view.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { ProductDetailsMainSlider, ProductDetailsThumbSlider } from 'src/app/shared/data/slider-option';
+import { ProductDetails } from 'src/app/shared/models/Product';
 
 @Component({
   selector: 'app-product-assets-view',
@@ -7,8 +8,9 @@ import { ProductDetailsMainSlider, ProductDetailsThumbSlider } from 'src/app/sha
   styleUrls: ['./product-assets-view.component.scss']
 })
 export class ProductAssetsViewComponent implements OnInit {
-  @Input() themeLogo = 'assets/images/a2z/logos/logo A2Z-01.png';
-  
+  @Input() themeLogo: string = 'assets/images/a2z/logos/logo A2Z-01.png';
+  @Input() productDetails : ProductDetails [];
+
   public ProductDetailsMainSliderConfig: any = ProductDetailsMainSlider;
   public ProductDetailsThumbConfig: any = ProductDetailsThumbSlider;
   constructor() { }
--- src/app/features/product/product.component.ts
+++ src/app/features/product/product.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from 'src/app/shared/services/product.service';
+import { Observable } from 'rxjs';
+import { ProductDetails } from 'src/app/shared/models/Product';
 
 @Component({
   selector: 'app-product',
@@ -6,10 +9,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product.component.scss']
 })
 export class ProductComponent implements OnInit {
-
-  constructor() { }
+  productDetails: Observable<ProductDetails[]>;
+  constructor(private _productService: ProductService) { }
 
   ngOnInit() {
+    this.productDetails = this._productService.getProductDetail(1);
   }
-   
 }
