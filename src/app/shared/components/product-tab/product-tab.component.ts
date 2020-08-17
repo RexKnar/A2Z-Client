82a8354763ed8489de63dcbdd855c382bfa0c076
Sobinjm <jmsobin@gmail.com>
2020-08-17 19:13:51 +0530

    product tab generic

--- src/app/shared/components/product-tab/product-tab.component.ts
+++ src/app/shared/components/product-tab/product-tab.component.ts
@@ -1,21 +1,18 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { ProductSlider } from '../../data/slider-option';
-import { Product } from '../../models/Product';
-import { Observable } from 'rxjs';
-import { ProductService } from '../../services/product.service';
+import { Component, OnInit, Input } from "@angular/core";
+import { ProductSlider } from "../../data/slider-option";
+import { ProductTab } from "../../models/ProductTab";
+import { Observable } from "rxjs";
 
 @Component({
-  selector: 'app-product-tab',
-  templateUrl: './product-tab.component.html',
-  styleUrls: ['./product-tab.component.scss']
+  selector: "app-product-tab",
+  templateUrl: "./product-tab.component.html",
+  styleUrls: ["./product-tab.component.scss"],
 })
 export class ProductTabComponent implements OnInit {
   public ProductSliderConfig: any = ProductSlider;
-  topProductData: Observable<Product[]>;
-  constructor(private _productService: ProductService) { }
+  @Input() tabData: Observable<ProductTab[]>;
 
-  ngOnInit(): void {
-    this.topProductData = this._productService.getProduct(5);
-  }
+  constructor() {}
 
+  ngOnInit(): void {}
 }
